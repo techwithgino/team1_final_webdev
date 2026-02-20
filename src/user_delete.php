8b15f96c35de797c79f1f4195d4ec7c0e880e59a
KatarinaVasic <amk1011817@student.hamk.fi>
2026-02-20 08:56:37 +0200

    Update user_delete.php

--- src/user_delete.php
+++ src/user_delete.php
@@ -2,18 +2,21 @@
 require 'auth_check.php';
 require 'db_connect.php';
 
+//admins only
+
 if ($_SESSION['role'] !== 'admin') {
     header("Location: cases_list.php");
     exit;
 }
 
 $id = $_GET['id'] ?? null;
+// if we got an id, go ahead and delete the user
 
 if ($id) {
     $stmt = $conn->prepare("DELETE FROM users WHERE id = ?");
     $stmt->bind_param("i", $id);
     $stmt->execute();
 }
-
+// after deleting just sending tghem back to dashboard
 header("Location: admin_portal.php");
 exit;
