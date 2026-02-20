509946f80dd9fdd6a037f948a632c3504aefe37a
KatarinaVasic <amk1011817@student.hamk.fi>
2026-02-20 08:25:03 +0200

    Update auth_check.php
    comments

--- src/auth_check.php
+++ src/auth_check.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
 
+// This is basically the "gatekeeper" for protected pages. 
+// If the user isn't logged in, they shouldn't be here.
+
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
+
+// If there's no user_id in the session, it means the user is not logged in
+// So we send them back to the login page.
     exit;
 }
 ?>
